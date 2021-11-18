c56020813bf876f4646601e609f929c39a430ee1
haiming <hesmaeilz01@gmail.com>
2021-11-18 14:33:53 -0500

    feature/admin-categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-  get '/about', to: 'about#index'
   
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  get '/about', to: 'about#index'
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:create, :index, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
