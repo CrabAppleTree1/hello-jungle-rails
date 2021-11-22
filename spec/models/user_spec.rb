a8b22f1ae573bb695e47994123dba1f0666dfba2
haiming <hesmaeilz01@gmail.com>
2021-11-21 23:44:39 -0500

    addToCart/ ProductDetails test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,6 @@ RSpec.describe User, type: :model do
 
   describe "Validations" do
 
-    it "is valid when given valid attributes" do
-      @user = User.new(first_name: "Void", last_name: "Test", email: "at@gmail.com", password: "123456", password_confirmation: "123456")
-      expect(@user).to be_valid
-    end
-
     it "is not valid when first_name is empty" do
       @user = User.new(first_name: nil, last_name: "Test", email: "at@gmail.com", password: "123456", password_confirmation: "123456")
       expect(@user).to_not be_valid
@@ -39,53 +34,10 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
-    it "is not valid when email is the same as another user" do
-      @user = User.new(first_name: "Void", last_name: "Test", email: "at@gmail.com", password: "123456", password_confirmation: "123456")
-      expect(@user).to be_valid
-      @user.save!
-      @user2 = User.new(first_name: "Void", last_name: "Test", email: "at@gMAIL.com", password: "123456", password_confirmation: "123456")
-      expect(@user2).to_not be_valid
-    end
-
     it "is not valid when password is less than 4 characters" do
       @user = User.new(first_name: "void", last_name: "test", email: "1@gmail.com", password: nil, password_confirmation: "123")
       expect(@user).to_not be_valid
     end
   end
-
-    describe '.authenticate_with_credentials' do
-      it 'should return if authentication is succesful' do
-        @user = User.new(first_name: 'Jonny', last_name: 'Oliver', email: 'the@gmail.com', password: 'fairyDust1', password_confirmation: 'fairyDust1')
-        @user.save
-        expect(User.authenticate_with_credentials('the@gmail.com', 'fairyDust1')).to eq(@user)
-      end
-
-      it 'should succesfully login a session' do
-        @user = User.create(first_name: 'Charlie', last_name: 'Oliver', email: 'CharlieOliver@gmail.com', password: 'tankandstuffandsjit', password_confirmation: 'tankandstuffandsjit')
-        expect(User.authenticate_with_credentials('CharlieOliver@gmail.com', 'tankandstuffandsjit')).to eq(@user)
-      end
-
-      it 'should not login/return nil if a email is incorrect' do
-        @user = User.create(first_name: 'Charlie', last_name: 'Oliver', email: 'CharlieOliver@gmail.com', password: 'tankandstuffandsjit', password_confirmation: 'tankandstuffandsjit')
-        expect(User.authenticate_with_credentials('chrisOliver@gmail.com', 'tankandstuffandsjit')).to eq(nil)
-      end
-
-      it 'should not login/return nil if a password is incorrect' do
-        @user = User.create(first_name: 'Charlie', last_name: 'Oliver', email: 'CharlieOliver@gmail.com', password: 'tankandstuffandsjit', password_confirmation: 'tankandstuffandsjit')
-        expect(User.authenticate_with_credentials('CharlieOliver@gmail.com', 'iforget')).to eq(nil)
-      end
-
-      it 'should succesfully login a session even if whitespace is added on edge of email' do
-        @user = User.create(first_name: 'Charlie', last_name: 'Oliver', email: 'CharlieOliver@gmail.com', password: 'tankandstuffandsjit', password_confirmation: 'tankandstuffandsjit')
-        expect(User.authenticate_with_credentials(' CharlieOliver@gmail.com  ', 'tankandstuffandsjit')).to eq(@user)
-      end
-
-      it 'should succesfully login a session if they mis-case their typed email' do
-        @user = User.create(first_name: 'Charlie', last_name: 'Oliver', email: 'CharlieOliver@gmail.com', password: 'tankandstuffandsjit', password_confirmation: 'tankandstuffandsjit')
-        expect(User.authenticate_with_credentials('CharlieOliver@gMAil.Com', 'tankandstuffandsjit')).to eq(@user)
-      end
-      
-    end
-
   
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,11 @@ require "capybara/poltergeist" # Add this line to require poltergeist
 # that JS driver to :poltergeist
 Capybara.javascript_driver = :poltergeist
 
+
+# Helper function to open product images
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
